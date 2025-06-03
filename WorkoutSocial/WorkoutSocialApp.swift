5fc8da871a88001304124d10137fc45a4ec8df89
Hammad Faiz <hammadfaiz172@gmail.com>
2025-06-03 19:42:13 -0400

    Initial Commit

---
+++ WorkoutSocial/WorkoutSocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutSocialApp.swift
+//  WorkoutSocial
+//
+//  Created by Hammad Faiz on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutSocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WorkoutSocialTests/WorkoutSocialTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutSocialTests.swift
+//  WorkoutSocialTests
+//
+//  Created by Hammad Faiz on 6/3/25.
+//
+
+import Testing
+@testable import WorkoutSocial
+
+struct WorkoutSocialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
